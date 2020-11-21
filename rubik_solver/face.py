0726baa67405fdf0d0a5599555e35478a10fb2c2
JosueDLA <josuedlavs@gmail.com>
2020-11-20 22:03:11 -0600

    Update face draw behavior

--- rubik_solver/face.py
+++ rubik_solver/face.py
@@ -3,48 +3,70 @@ import cv2
 
 
 class Face(tuple):
-    def __new__(self, width):
+    def __new__(self, height, width):
+        self.height = height
         self.width = width
-        return tuple.__new__(Face, (self.width,))
+        return tuple.__new__(Face, (self.width, self.height))
 
-    def get_coordinates(self):
+    def get_coordinates(self, porcentage=40):
         faces = []
-        y = []
-        x = []
+        faces_x = []
+        faces_y = []
 
         margin = 1
-        initial = 40
-        available = 80
-        border = (100 - available)/2
-        size = (available/4) - (margin*2)
+        available_x = porcentage
+        margin_x = (100 - available_x)/2
+        size = (available_x/4) - (margin*2)
 
-        first = border + margin
+        # Biggest axis
+        axis = self.width if self.width > self.height else self.height
+
+        # Calculate x axis values
+        first = margin_x + margin
 
         for i in range(4):
-            x.append(first)
+            faces_x.append(first)
             second = first + size
-            x.append(second)
+            faces_x.append(second)
             first = second + (margin*2)
             second = first + size
 
-        first = initial + margin
+        # Calculate y axis values
+        first = 100 + margin
 
         for i in range(3):
-            y.append(first)
+            faces_y.append(first)
             second = first + size
-            y.append(second)
+            faces_y.append(second)
             first = second + (margin*2)
             second = first + size
 
-        x = [int((e/100) * self.width) for e in x]
-        y = [int((e/100) * self.width) for e in y]
+        # Calculate extra space in x axis
+        extra_x = (faces_x[-1]/100) * axis
+        extra_x = (self.width - extra_x)
+        extra_x = 0 if extra_x > 0 else abs(extra_x)
+
+        # Center faces in x axis
+        if extra_x != 0:
+            center_x = (margin_x/100) * self.width
+            extra_x = center_x - extra_x
+            print(extra_x)
+
+        # Calculate extra space in y axis
+        extra_y = (faces_y[-1]/100) * axis
+        extra_y = (self.height - extra_y)
+        extra_y = 0 if extra_y > 0 else abs(extra_y)
+
+        # Calculate faces coordinates according to resolution
+        faces_x = [int(((e/100) * axis) - extra_x) for e in faces_x]
+        faces_y = [int(((e/100) * axis) - extra_y) for e in faces_y]
 
         # Faces U L F R B D
-        faces.append(((x[2], y[0]), (x[3], y[1])))
-        faces.append(((x[0], y[2]), (x[1], y[3])))
-        faces.append(((x[2], y[2]), (x[3], y[3])))
-        faces.append(((x[4], y[2]), (x[5], y[3])))
-        faces.append(((x[6], y[2]), (x[7], y[3])))
-        faces.append(((x[2], y[4]), (x[3], y[5])))
+        faces.append(((faces_x[2], faces_y[0]), (faces_x[3], faces_y[1])))
+        faces.append(((faces_x[0], faces_y[2]), (faces_x[1], faces_y[3])))
+        faces.append(((faces_x[2], faces_y[2]), (faces_x[3], faces_y[3])))
+        faces.append(((faces_x[4], faces_y[2]), (faces_x[5], faces_y[3])))
+        faces.append(((faces_x[6], faces_y[2]), (faces_x[7], faces_y[3])))
+        faces.append(((faces_x[2], faces_y[4]), (faces_x[3], faces_y[5])))
 
         return faces
